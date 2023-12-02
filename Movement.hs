f87be9547295b8352dc8a6a3fad86a3605ac63b0
mgarbowski <mikolaj.garbowski@gmail.com>
2023-12-02 18:41:45 +0100

    add comment

--- Movement.hs
+++ Movement.hs
@@ -9,7 +9,7 @@ moveDirection directionStr gameState =
     Just direction -> case lookup direction (directions (currentRoom gameState)) of
       Just otherRoom ->
         case Map.lookup (roomName otherRoom) (roomStates gameState) of
-           Just room -> return gameState {currentRoom = room}
+           Just room -> return gameState {currentRoom = room}  -- set currentRoom to the updated value from gameState, todo rethink this
            _ -> return gameState
       Nothing -> do
         putStrLn "You can't go that way"
