f0b6b36b48f9f6a0f6febc14983974b190340403
mgarbowski <mikolaj.garbowski@gmail.com>
2023-12-02 14:11:06 +0100

    initial commit

---
+++ Instructions.hs
@@ -0,0 +1,9 @@
+module Instructions where
+
+instructionsText = [
+    "Available commands are:",
+    "",
+    "instructions  -- to see these instructions.",
+    "quit          -- to end the game and quit.",
+    ""
+    ]
