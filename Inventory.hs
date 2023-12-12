58c676c71ac19db1053cf9f9dbe27f949b800c1b
mgarbowski <mikolaj.garbowski@gmail.com>
2023-12-12 22:37:33 +0100

    move implementation of inventory related functionality to a separate module

---
+++ Inventory.hs
@@ -0,0 +1,46 @@
+module Inventory where
+import qualified Data.Map as Map
+import Data.List
+import GameState
+import Rooms
+import Interactables
+
+displayInventory :: GameState -> IO GameState
+displayInventory gameState =
+  let pairs = Map.elems (inventory gameState)
+      lines = map (\(item, count) -> "- " ++ name item ++ " x" ++ show count) pairs
+      text = intercalate "\n" lines
+  in putStrLn text >> return gameState
+
+-- Function to take an item from the current room and add it to the inventory
+takeItem :: String -> GameState -> IO GameState
+takeItem itemName gameState =
+  case find (\item -> name item == itemName) (roomItems (currentRoom gameState)) of
+    Just item -> do
+      let currentInventory = inventory gameState
+      let updatedInventory = addItemToInventory item currentInventory
+      let currentRoomStates = roomStates gameState
+      let currentRoomName = roomName $ currentRoom gameState
+      let updatedRoom = removeItemFromRoom item (currentRoom gameState)
+      let updatedRoomStates = Map.insert currentRoomName updatedRoom currentRoomStates
+      return gameState {
+        currentRoom = updatedRoom,
+        inventory = updatedInventory,
+        roomStates = updatedRoomStates
+      }
+    Nothing -> putStrLn "I don't see that here" >> return gameState
+
+
+-- helper functions
+addItemToInventory :: Interactable -> Map.Map String (Interactable, Int) -> Map.Map String (Interactable, Int)
+addItemToInventory newItem inventory =
+  case Map.lookup (name newItem) inventory of
+    Just (i, count) -> Map.insert (name newItem) (newItem, count+1) inventory
+    Nothing -> Map.insert (name newItem) (newItem, 1) inventory
+
+
+removeItemFromRoom :: Interactable -> Room -> Room
+removeItemFromRoom item room =
+  let prevItems = roomItems room
+      newItems = filter (\i -> name i /= name item) prevItems
+  in room {roomItems = newItems}
