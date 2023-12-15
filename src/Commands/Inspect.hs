8944ca00c353f82763ab97490af6a15bb142202e
maksym <01178511@pw.edu.pl>
2023-12-15 16:16:25 +0100

    add items to inventory on inspecting the coat and broken door

--- src/Commands/Inspect.hs
+++ src/Commands/Inspect.hs
@@ -2,10 +2,17 @@ module Commands.Inspect where
 import Data.List
 import GameState
 import Interactables
+import Items
+import Commands.Inventory
 
 handleInspect :: String -> GameState -> IO GameState
 handleInspect entityName gameState = do
   case find (\i -> name i == entityName) (allInteractables gameState) of
-    Just entity -> (putStrLn $ description entity)
-    _ -> putStrLn "I don't see that here"
-  return gameState
+    Just entity -> do
+      (putStrLn $ description entity)
+      if entityName == "coat"
+      then return gameState { inventory = addItemToInventory lockerRoomKey (inventory gameState) }
+      else if entityName == "broken door"
+      then return gameState { inventory = addItemToInventory lockerRoomKey (inventory gameState) }
+      else return gameState
+    _ -> putStrLn "I don't see that here" >> return gameState
