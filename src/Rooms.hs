bc165cd25615690457589f4bf2dcb73a1b428584
mgarbowski <mikolaj.garbowski@gmail.com>
2023-12-15 14:02:26 +0100

    fix locker room description

--- src/Rooms.hs
+++ src/Rooms.hs
@@ -38,9 +38,9 @@ lockerRoom = Room {
                     "of lockers fill the space, closed and orderly. However, one locker catches\n" ++
                     "your eye - slightly open, a hint of something inside. It seems like the owner\n" ++
                     "forgot to lock it properly\n" ++
-                    "To the north, a heavy metal door labeled \"Security\" presents itself to you\n" ++
+                    "To the north, a heavy metal door labeled \"Security\" and a modern keypad next to it present themselves to you\n" ++
                     "To the east, you see another door with no labels as to where it leads\n",
-  roomHint = "You can interact with the locker, go north or go east, use `examine north door` to get more information.",
+  roomHint = "You can interact with the locker, go north or go east, inspect the keypad next to security doors to find out more.",
   roomItems = [lockerRoomKey, coat, labShoes],
   interactables = [keypad],
   directions = Map.fromList [
