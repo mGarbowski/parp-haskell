8f66d9b0628aa2c1304fb861d20a50360668636f
maksym <01178511@pw.edu.pl>
2023-12-16 16:20:11 +0100

    special inspect cases for dynamic descriptions

--- src/Commands/Inspect.hs
+++ src/Commands/Inspect.hs
@@ -5,9 +5,18 @@ import Interactables
 import Items
 import Util
 import qualified Data.Map as Map
+import Data.Maybe (fromJust)
 
 handleInspect :: String -> GameState -> IO GameState
-handleInspect entityName gameState = do
+handleInspect entityName gameState
+  | entityName == name toolChest = handleToolChestInspect gameState
+  | entityName == name locker = handleLockerInspect gameState
+  | entityName == name compartment = handleCompartmentInspect gameState
+  | otherwise = handleCompartmentInspect gameState
+
+
+handleSimpleInspect :: String -> GameState -> IO GameState
+handleSimpleInspect entityName gameState = do
   case find (\i -> name i == entityName) (allInteractables gameState) of
     Just entity -> do
       (putStrLn $ description entity)
@@ -20,4 +29,27 @@ handleInspect entityName gameState = do
         True -> return gameState
         False -> return gameState { inventory = addItemToInventory smallKey (inventory gameState) }
       else return gameState
-    _ -> putStrLn "I don't see that here" >> return gameState
+    _ -> do putStrLn "I don't see that here"
+            return gameState
+
+
+handleCompartmentInspect :: GameState -> IO GameState
+handleCompartmentInspect gameState =
+  case lockerCompartmentBlocked gameState of
+    True -> putStrLn "The bottom compartment is locked. There is a keyhole, but where is the key?" >> return gameState
+    False -> handleSimpleInspect (name compartment) gameState
+
+handleLockerInspect :: GameState -> IO GameState
+handleLockerInspect gameState =
+  let lockerContents = fromJust $ Map.lookup (name locker) (containerContents gameState) in
+    case coat `elem` lockerContents of
+      False -> do
+               putStr ("A locker for storing personal items" ++
+                      "\nThe locker reveals two compartments, in the upper part, where you took the coat from and" ++
+                      "\nanother compartment below it.\n")
+               return gameState
+      True -> handleSimpleInspect (name locker) gameState
+
+
+handleToolChestInspect :: GameState -> IO GameState
+handleToolChestInspect gameState = handleSimpleInspect (name toolChest) gameState -- todo!
